153b1e7c940680934003b00741254fabd980df15
tlsh0 <a.tolesh@bk.ru>
2024-01-11 16:48:16 +0600

    color changes and logo

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -33,7 +33,7 @@ export const SidebarItem = ({
             type="button"
             className={cn(
                 "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
-                isActive && "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+                isActive && "text-red-700 bg-red-200/20 hover:bg-red-200/20 hover:text-red-700"
             )}
         >
             <div className="flex items-center gap-x-2 py-4">
@@ -41,14 +41,14 @@ export const SidebarItem = ({
                     size={22}
                     className={cn(
                         "text-slate-500",
-                        isActive && "text-sky-700"
+                        isActive && "text-red-700"
                     )}
                 />
                 {label}
             </div>
             <div 
                 className={cn(
-                    "ml-auto opacity-0 border-2 border-sky-700 h-full transition-all",
+                    "ml-auto opacity-0 border-2 border-red-700 h-full transition-all",
                     isActive && "opacity-100"
                 )}
             />
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -24,4 +24,26 @@ async function main() {
     }
 }
 
+// async function seedCourses() {
+//     try {
+//         await database.courses.createMany({
+//             data: [
+//                 { name: "Computer Science" },
+//                 { name: "Music" },
+//                 { name: "Fitness" },
+//                 { name: "Photography" },
+//                 { name: "Accounting" },
+//                 { name: "Engineering" },
+//                 { name: "Filming" },
+//             ]
+//         });
+
+//         console.log("Success")
+//     } catch (error) {
+//         console.log("Error seeding the database categories", error);
+//     } finally {
+//         await database.$disconnect();
+//     }
+// }
+
 main();
